c0aab3ddce6417ceb536d01089d9ec6292820c53
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-01-31 14:03:18 -0300

    chore: modifica regra eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     '@typescript-eslint/promise-function-async': 'off',
     '@typescript-eslint/method-signature-style': 'off',
     '@typescript-eslint/no-invalid-void-type': 'off',
-    '@typescript-eslint/consistent-type-imports': 'off'
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off'
   }
 }
